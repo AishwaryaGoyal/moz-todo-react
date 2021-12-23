bb053083ffee6dd8fecfa7b296e463f45b324c6a
Aishwarya Goyal <aishwaryag@crowdstaffing.com>
2021-12-23 19:44:06 +0530

    MDN Todo tutorial on React - complete

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+function Modal() {
+  return (
+    <div className="modal">
+      Are you sure?
+      <br />
+      <button className="confirm-btn">Yes</button>
+      <button className="cancel-btn">No</button>
+    </div>
+  );
+}
+
+export default Modal;
